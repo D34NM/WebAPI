97cc47593cd026e85c82218fb48b2b83a657bc0e
Dean Milojevic <milojevicdean@gmail.com>
2017-03-12 19:11:18 +0100

    Added new unit tests and updated the packages in use.

---
+++ B64Comparer.Integration.Tests/IntegrationFacts.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace B64Comparer.Integration.Tests
+{
+	[TestClass]
+	public class IntegrationFacts
+	{
+		private TestServer _server;
+
+		[TestInitialize]
+		public void TestMethod1()
+		{
+		}
+
+		[TestMethod]
+		public async Task Should_ReturnEqual_When_DataIsValid()
+		{
+			StringContent content = new StringContent("adadaasdaskdjaka");
+			var response = await new HttpClient().PutAsync("http://localhost/v1/diff/1/left", content);
+
+			Assert.IsNotNull(response);
+		}
+	}
+}
